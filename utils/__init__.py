30116206ae06851fe487b8544af524b63e934695
Alex <alexsousacosta78@gmail.com>
2026-09-23 16:15:03 -0300

    feat: criado as validacoes para ser utilizado nos services.

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import database
-from . import cadastros
-from . import listar_produtos
-from . import adicionar_produto
-from . import pedido
--- utils/validacoes.py
+++ utils/validacoes.py
@@ -1,40 +1,43 @@
-def ler_preco():
-    while True:
-        try:
-            preco = float(input("Preço: "))
-
-            if preco < 0:
-                print("O preço não pode ser negativo.")
-                continue
-
-            return preco
-        
-        except ValueError:
-            print("Digite um número válido.")
+def validar_preco(preco):
+   if not isinstance(preco, (int, float)):
+       raise TypeError("Preço deve ser um número.")
+   if preco < 0:
+       raise ValueError("Preço não pode ser negativo.")
+   return preco
 
 
-
-def ler_quantidade():
-    while True:
-        try:
-            quantidade = int(input("Quantidade: "))
-
-            if quantidade < 0:
-                print("Digite um número válido.")
-                continue
-
-            return quantidade
-        
-        except ValueError:
-            print("Digite um número válido.")
+def validar_quantidade(qtd):
+   if not isinstance(qtd, int):
+       raise TypeError("Quantidade deve ser um número inteiro")
+   if qtd < 0:
+       raise ValueError("Quantidade não pode ser negativo.")
+   return qtd
 
 
+def validar_email(email: str):
+    if not isinstance(email, str) or "@" not in email or "." not in email:
+        raise TypeError("Email inválido")
+    return email
 
-def ler_email():
-    while True:
-        email = str(input("Email: ")).strip()
 
-        if "@" in email and "." in email:
-            return email
-        
-        print("Email inválido")
+def validar_nome(nome):
+    if not isinstance(nome, str) or not nome.strip():
+        raise TypeError("Nome não pode ser vazio")
+    if len(nome) > 25:
+        raise ValueError("Nome muito longo (máximo 25 caracteres).")
+    return nome
+
+
+def validar_id(id):
+    if not isinstance(id, int):
+        raise TypeError("Id deve ser um número inteiro")
+    if id < 0:
+        raise ValueError("O id não pode ser um número negativo")
+    return id
+
+
+def validar_telefone(telefone):
+    numeros = "".join(c for c in telefone if c.isdigit())
+    if len(numeros) not in (10, 11):
+        raise ValueError("Telefone inválido")
+    return telefone
